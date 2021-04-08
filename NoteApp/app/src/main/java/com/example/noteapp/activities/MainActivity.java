3e2ca642bd1431245015d1767af98ef133ba049e
biteackerman <rizqikurnia98@gmail.com>
2021-04-08 19:26:24 +0700

    Database progres

--- NoteApp/app/src/main/java/com/example/noteapp/activities/MainActivity.java
+++ NoteApp/app/src/main/java/com/example/noteapp/activities/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.noteapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.noteapp.R;
-import com.example.noteapp.activities.CreateNoteActivity;
+import com.example.noteapp.database.NotesDatabase;
+import com.example.noteapp.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +35,30 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+
+        getNotes();
+    }
+
+    private void getNotes(){
+
+        @SuppressLint("StaticFieldLeak")
+        class  GetNoteTask extends AsyncTask<Void, Void, List<Note>>{
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase
+                        .getDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+
+        }
+
+        new GetNoteTask().execute();
     }
 }
--- NoteApp/app/src/main/java/com/example/noteapp/database/NotesDatabase.java
+++ NoteApp/app/src/main/java/com/example/noteapp/database/NotesDatabase.java
@@ -15,7 +15,7 @@ public abstract  class NotesDatabase extends RoomDatabase {
 
     private static NotesDatabase notesDatabase;
 
-    private static synchronized NotesDatabase getDatabase(Context context){
+    public static synchronized NotesDatabase getDatabase(Context context){
         if(notesDatabase == null) {
             notesDatabase = Room.databaseBuilder(
                     context,
